c4813576d8f85234cbb2389db0f6e90497c3d84b
00n3l5on00 <caleronelson05@gmail.com>
2023-11-17 21:42:08 -0500

    Ejercicios

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea 3 - Presentación</title>
+</head>
+<body>
+
+    <h1>Bienvenido a mi Tarea 3</h1>
+
+    <p>Esta tarea incluye varios ejercicios que abarcan HTML, CSS, JavaScript, y PHP. A continuación, encontrarás un menú con enlaces a cada ejercicio:</p>
+
+    <ul>
+        <li><a href="ejercicio01/">Ejercicio 1: Menú Desplegable</a></li>
+        <li><a href="ejercicio02/">Ejercicio 2: Formulario de Contacto</a></li>
+        <li><a href="ejercicio03/">Ejercicio 3: Contador de Visitas en PHP</a></li>
+        <li><a href="ejercicio04/">Ejercicio 4: Juego de Adivinanzas</a></li>
+        <li><a href="ejercicio05/">Ejercicio 5: Galería de Imágenes</a></li>
+        <li><a href="ejercicio06/">Ejercicio 6: Sistema de Login</a></li>
+    </ul>
+
+</body>
+</html>
